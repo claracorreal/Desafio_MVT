0950dda8c644f3ace8bb7d1c4e1025b2b15c4203
Clara Correa Luna <claracorrealu@gmail.com>
2022-06-30 16:06:16 -0300

    desafio final

--- flia_app/models.py
+++ flia_app/models.py
@@ -1,6 +1,9 @@
 from django.db import models
+from datetime import datetime, date
 
 class Familia(models.Model):
     nombre = models.CharField(max_length=40)
     edad = models.IntegerField()
+    cumple = models.DateField(auto_now_add=False, auto_now=False, blank=True)
+    email = models.EmailField()
 
